906afc0951be9a78861447740021d6d1238df290
Debabrata23 <debabratash0@gmail.com>
2022-09-30 16:17:59 +0530

    Change in home app and package v1

--- src/components/TopNavigation.js
+++ src/components/TopNavigation.js
@@ -5,9 +5,9 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Home from './Home/Home';
-
+import { useLocation } from 'react-router-dom';
 const TopNavigation = () => {
-
+   const locationVal=useLocation();
   return (
 
     <Row className='navRow'>
@@ -16,9 +16,9 @@ const TopNavigation = () => {
       </Col>
       <Col xs={9} className="navAnimate">
         <div className="headerDiv">
-          <Button className={window.location.pathname=='/'? 'activeDiv':'lnkheaderNav'}  href="/">Home</Button>
-          <Button className={window.location.pathname=='/Certifications'? 'activeDiv':'lnkheaderNav'} href="/Certifications">Certifications & Skills</Button>
-          <Button className={window.location.pathname=='/WorkExperience'? 'activeDiv':'lnkheaderNav'} href="/WorkExperience">Work Experience</Button>
+          <Button className={locationVal.pathname=='/my-profile'? 'activeDiv':'lnkheaderNav'}  href="/">Home</Button>
+          <Button className={locationVal.pathname=='/Certifications'? 'activeDiv':'lnkheaderNav'} href="/Certifications">Certifications & Skills</Button>
+          <Button className={locationVal.pathname=='/WorkExperience'? 'activeDiv':'lnkheaderNav'} href="/WorkExperience">Work Experience</Button>
      
          
         </div>
